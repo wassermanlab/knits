1c9ccc2cb61c75b4467c8e66d26caad5efacde62
masvsal <95512439+masvsal@users.noreply.github.com>
2022-08-10 14:02:51 -0700

    commented out benchmarking methods @ main import script.

--- current/import/scripting/main.py
+++ current/import/scripting/main.py
@@ -10,8 +10,7 @@ import time
 import pandas as pd
 
 gene_names = cfg.FIBROBLAST_TO_NEURON + cfg.FIBROBLAST_TO_ENDOTHELIAL + cfg.FIBROBLAST_TO_THYMUS + cfg.FIBROBLAST_TO_HSC + cfg.FIBROBLAST_TO_SERTOLI
-print(gene_names)
-
+""" 
 def benchmark_iteratively(gene_names_added):
     ensembl_timing = []
     jaspar_timing = []
@@ -46,11 +45,11 @@ def benchmark_once(gene_names):
     jaspar_timing = []
     other_timing = []
     num_genes = [len(gene_names)]
-    """ start = time.time()
+    start = time.time()
     er.request_ensembl(gene_names=gene_names)
     end = time.time()
     ensembl_timing += [end - start]
-    #import and save information annotations with gene names from each of the following databases """
+    #import and save information annotations with gene names from each of the following databases
     start = time.time()
     jp.request_jaspar(gene_names=gene_names)
     end = time.time()
@@ -65,13 +64,12 @@ def benchmark_once(gene_names):
     other_timing += [end - start]
     df = pd.DataFrame(data={'num_genes':num_genes,'ensembl':ensembl_timing, 'jaspar':jaspar_timing, 'other':other_timing})
     df.to_csv('current/analysis/similarity_data/similarity_data.csv')
-""" 
+"""
+#import sequence
 er.request_ensembl(gene_names=gene_names)
 jp.request_jaspar(gene_names=gene_names)
 gp.parse_go_annotations(gene_names=gene_names)
 tp.parse_tfclass(gene_names=gene_names)
 sr.request_string(gene_names=gene_names)
 br.request_biogrid(gene_names=gene_names)
-cr.request_CTDbase(gene_names=gene_names) """
-
-cr.get_go_annotations(gene_names=gene_names).to_csv('current/data/gene_annotations/ctdbase/automated_go_annotations.csv')
+cr.request_CTDbase(gene_names=gene_names)
